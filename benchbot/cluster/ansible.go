e17e31edd2ec9c72660f9bb18da9da58d0f331ed
cwen0 <cwenyin0@gmail.com>
2018-02-26 22:33:09 +0800

    fix bug

--- benchbot/cluster/ansible.go
+++ benchbot/cluster/ansible.go
@@ -13,7 +13,7 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	. "github.com/pingcap/octopus/benchbot/common"
-	"github.com/pingcap/schrodinger/util"
+	"github.com/pingcap/octopus/benchbot/util"
 )
 
 const (
