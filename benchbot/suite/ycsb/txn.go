bc43ef2cbb9c01f187eb12d6fcad1e7c1942c0a4
Breezewish <me@breeswish.org>
2018-06-07 16:13:26 +0800

    invoke kv delete

--- benchbot/suite/ycsb/txn.go
+++ benchbot/suite/ycsb/txn.go
@@ -6,10 +6,8 @@ import (
 	"github.com/juju/errors"
 
 	"github.com/pingcap/tidb/kv"
-	"github.com/pingcap/tidb/sessionctx/stmtctx"
 	"github.com/pingcap/tidb/store/tikv"
 	"github.com/pingcap/tidb/tablecodec"
-	"github.com/pingcap/tidb/types"
 
 	goctx "golang.org/x/net/context"
 )
@@ -42,26 +40,37 @@ func (c *txnKV) ReadRow(id uint64) (bool, error) {
 func (c *txnKV) InsertRow(id uint64, fields []string) error {
 	// Simulate TiDB data
 	rowID := tablecodec.EncodeRowKeyWithHandle(1, int64(id))
-	cols := make([]types.Datum, len(fields))
-	for i, v := range fields {
-		cols[i].SetString(v)
-	}
-
-	rowData, err := tablecodec.EncodeRow(&stmtctx.StatementContext{}, cols, colIDs, nil, nil)
-	if err != nil {
-		return err
-	}
-
 	tx, err := c.db.Begin()
 	if err != nil {
 		return err
 	}
 
-	if err := tx.Set(rowID, rowData); err != nil {
+	if err := tx.Delete(rowID); err != nil {
 		return err
 	}
-
 	return tx.Commit(goctx.Background())
+	/*
+		cols := make([]types.Datum, len(fields))
+		for i, v := range fields {
+			cols[i].SetString(v)
+		}
+
+		rowData, err := tablecodec.EncodeRow(&stmtctx.StatementContext{}, cols, colIDs, nil, nil)
+		if err != nil {
+			return err
+		}
+
+		tx, err := c.db.Begin()
+		if err != nil {
+			return err
+		}
+
+		if err := tx.Set(rowID, rowData); err != nil {
+			return err
+		}
+
+		return tx.Commit(goctx.Background())
+	*/
 }
 
 func setupTxnKV(pdAddr string) (Database, error) {
